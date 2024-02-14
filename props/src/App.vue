6b8c62a30fde2383767945b43a87f0803c32ddd0
premsaitoram <t.prem256@gmail.com>
2024-02-14 18:33:40 +0530

    add files

---
+++ props/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from "vue";
+import ListItem from "./components/ListItem.vue";
+
+let database = ref([
+  {
+    name: "Jane",
+    deparment: "Cricket",
+    salary: 300000,
+  },
+  {
+    name: "Stark",
+    deparment: "Movies",
+    salary: 400000,
+  },
+  {
+    name: "John",
+    deparment: "Engineering",
+    salary: 30000,
+  },
+]);
+function deleteCard(position) {
+  database.value.splice(position, 1);
+}
+</script>
+
+<template>
+  <main>
+    <ListItem 
+    v-for="(item, index) in database"
+    :name="item.name" 
+    :department="item.deparment" 
+    :salary="item.salary" 
+    :position="index" 
+    v-on:deleteItem="deleteCard"
+    />
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+</style>
---
+++ props/src/components/ListItem.vue
@@ -0,0 +1,34 @@
+<script setup>
+defineProps({
+    name: String,
+    department: String,
+    salary: Number,
+    position: Number
+})
+
+</script>
+
+<template>
+    <div class="container">
+        <h2>Name:{{name}}</h2>
+        <p>Deparment:{{department}}</p>
+        <p>Salary:{{salary}}</p>
+        <p>Position:{{position}}</p>
+        <button @click="$emit('deleteItem', position)">Delete Item</button>
+    </div>
+</template>
+
+<style scoped>
+h2 {
+	padding: 0;
+	margin: 0;
+}
+.container {
+	border: 1px solid rgb(124, 191, 202);
+	width: 300px;
+	margin: 1em auto 1em auto;
+	padding: 1em;
+	border-radius: 5px;
+	font-size: 1.3em;
+}
+</style>
